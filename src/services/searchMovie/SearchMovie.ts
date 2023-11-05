f753f1cce7e50518b83b021bbc575c4760198fdc
baotran310702 <tranbao1414@gmail.com>
2023-11-05 23:30:31 +0700

    update search

---
+++ src/services/searchMovie/SearchMovie.ts
@@ -0,0 +1,17 @@
+import supabase from "../../config/supabase/supabase";
+import MovieGetDTO from "../../dtos/MovieGetDTO";
+
+export default async function SearchMovie(search: string) {
+  let { data, error } = await supabase
+    .from("tbl_movie")
+    .select("*")
+    .like("title", `%${search}%`);
+
+  if (error) {
+    return null;
+  }
+  if (data) {
+    console.log(data);
+    return data;
+  }
+}
