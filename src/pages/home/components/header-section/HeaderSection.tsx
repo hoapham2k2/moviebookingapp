642974fadfe475a0b58b8238920ea8b3c2de0f8e
baotran310702 <tranbao1414@gmail.com>
2023-11-07 23:06:45 +0700

    update search feature and home

--- src/pages/home/components/header-section/HeaderSection.tsx
+++ src/pages/home/components/header-section/HeaderSection.tsx
@@ -4,6 +4,7 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { IoFilterOutline, IoReloadCircle } from "react-icons/io5";
 import store from "../../../../config/storage/IonicStorage";
 import SearchMovie from "../../../../services/searchMovie/SearchMovie";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -52,8 +53,13 @@ const HeaderSection = (props: Props) => {
   }, []);
 
   return (
-    <div className={`h-32 sticky top-0 left-0 right-0 px-4 py-3 bg-black `}>
-      <div className="grid grid-cols-12 h-1/2">
+    <div className={`h-32 sticky top-0 left-0 right-0 px-4 py-3 bg-black z-50`}>
+      <div
+        className="grid grid-cols-12 h-1/2"
+        onClick={() => {
+          setShowResult(false);
+        }}
+      >
         {/* avatar section */}
         <div className="col-span-2 flex justify-start items-center">
           <div className="w-10 h-10 rounded-full bg-gray-300"></div>
@@ -88,7 +94,7 @@ const HeaderSection = (props: Props) => {
       {/* search section */}
       <div className="mt-1 grid grid-cols-12 h-1/2 w-full">
         <div className="relative col-span-12 flex justify-center items-center w-full">
-          <div className="w-full h-10 bg-zinc-700 rounded-xl grid grid-cols-12 px-6">
+          <div className="w-full h-10 bg-zinc-700 rounded-xl grid grid-cols-12 px-6 items-center justify-items-center">
             <div className="col-span-1 flex justify-center items-center">
               <div className="text-2xl">
                 <BiSearchAlt2 />
@@ -96,43 +102,38 @@ const HeaderSection = (props: Props) => {
             </div>
             <div className="col-span-10 flex justify-between items-center w-full ">
               <div className="flex justify-between items-center">
-                <input
-                  type="text"
-                  placeholder="Search by movies or cinema"
-                  className="w-full h-full px-4 bg-transparent outline-none text-sm border border-transparent focus:ring-transparent focus:ring-0 "
-                  onChange={handleSearch}
-                  onBlur={() => setShowResult(false)}
-                />
-              </div>
-              {isSearching && (
-                <IoReloadCircle className="w-6 h-6 transition-all animate-spin duration-500 mr-4" />
-              )}
-            </div>
-
-            {/* filter section */}
-            <div className="col-span-1 flex justify-center items-center">
-              <div className="text-2xl">
-                <IoFilterOutline />
-              </div>
-            </div>
-          </div>
-          {/**Search Result */}
-          {showResult && (
-            <div className="absolute rounded-xl text-black bg-slate-300 border border-slate-400 w-full top-12 max-h-96 overflow-scroll">
-              {resut.map((item: any, index: number) => {
-                return (
-                  <SearchItem
-                    key={index}
-                    title={item.title}
-                    thumbnail={item.thumbnail}
-                    duration={item.duration}
-                    language={item.country}
-                    genre={item.category}
+                <div className="">
+                  <input
+                    type="text"
+                    placeholder="Search by movies or cinema"
+                    className="w-full h-full px-4 bg-transparent outline-none text-sm border border-transparent focus:ring-transparent focus:ring-0 "
+                    onChange={handleSearch}
                   />
-                );
-              })}
+                </div>
+              </div>
             </div>
-          )}
+            {isSearching && (
+              <IoReloadCircle className="w-6 h-6 transition-all animate-spin duration-500 mr-4" />
+            )}
+            {/**Search Result */}
+            {showResult && (
+              <div className="absolute rounded-xl text-black bg-slate-300 border border-slate-400 w-full top-12 max-h-96 overflow-scroll">
+                {resut.map((item: any, index: number) => {
+                  return (
+                    <SearchItem
+                      key={index}
+                      id={item.id}
+                      title={item.title}
+                      thumbnail={item.thumbnail}
+                      duration={item.duration}
+                      language={item.country}
+                      genre={item.category}
+                    />
+                  );
+                })}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -142,6 +143,7 @@ const HeaderSection = (props: Props) => {
 //Result compoenent
 
 interface SearchItemComponent {
+  id: number;
   title: string;
   thumbnail: string;
   duration: string;
@@ -150,6 +152,7 @@ interface SearchItemComponent {
 }
 
 const SearchItem: React.FC<SearchItemComponent> = ({
+  id,
   title,
   thumbnail,
   duration,
@@ -157,19 +160,26 @@ const SearchItem: React.FC<SearchItemComponent> = ({
   genre,
 }) => {
   return (
-    <div className="flex px-4 py-2 gap-4 ">
-      <div>
-        <img src={thumbnail} className="w-14 h-18"></img>
-      </div>
-      <div className="flex flex-col justify-between">
-        <p className="text-base font-bold">{title}</p>
+    <Link to={`/home/${id}`}>
+      <div
+        className="flex px-4 py-2 gap-4 items-center "
+        onClick={() => {
+          console.log(id);
+        }}
+      >
         <div>
-          <p className="text-xs ">Thời lượng: {duration}</p>
-          <p className="text-xs">Ngôn ngữ: {language}</p>
-          <p className="text-xs">Thể loại: {genre}</p>
+          <img src={thumbnail} className="w-20 h-auto"></img>
+        </div>
+        <div className="flex flex-col justify-between">
+          <p className="text-base font-bold">{title}</p>
+          <div>
+            <p className="text-xs ">Thời lượng: {duration}</p>
+            <p className="text-xs">Ngôn ngữ: {language}</p>
+            <p className="text-xs">Thể loại: {genre}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
