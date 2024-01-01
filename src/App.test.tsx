f03e7e002e21051372747bd773f38ca383e0ce95
Hà Danh Tuấn <82920615+hadanhtuan@users.noreply.github.com>
2024-01-01 22:41:07 +0700

    fix app.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,5 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
-
+  const { baseElement } = render(<App />);
+  expect(baseElement).toBeDefined();
 });
